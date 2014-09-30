df06f48262e8800223011fe6f69db51de6f57fb5
SABATIER Boris <sabatier.boris@gmail.com>
2014-09-30 12:26:55 +1100

    Add the script to lunch localy test and check

---
+++ runTest.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#pip install pep8 > /dev/null
+#pip install pylint > /dev/null
+#pip install nose > /dev/null
+#python setup.py develop --upgrade > /dev/null
+
+set -e
+
+pep8 pyramidapp setup.py wsgi.py
+pylint pyramidapp setup.py wsgi.py
+nosetests
+initialize_bd pyramidapp/config/development.ini
+pserve pyramidapp/config/development.ini --reload
