f43a7ec94e0de92fcada7f241e979c6b0a004ce4
SABATIER Boris <sabatier.boris@gmail.com>
2014-11-28 10:23:47 +0100

    Ad the secret into config file

--- pyramidapp/__init__.py
+++ pyramidapp/__init__.py
@@ -4,20 +4,26 @@ __init__ file of the pyramid application
 Contain the main function
 """
 from pyramid.config import Configurator
+from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.response import Response
 from pyramid.session import SignedCookieSessionFactory
 
 from sqlalchemy import engine_from_config
 
 from .models import DB_SESSION, BASE
+from .models.security import groups_finder
+from .models.security import ACL
 
 
-def hello_world(request):
+class RootFactory(object):
+    # pylint: disable=R0903
     """
-    Example of a request function
+    RootFactory for ACL setting
     """
-    request = request  # Remove W0613
-    return Response('Hello!')
+    __acl__ = ACL()
+
+    def __init__(self, request):
+        pass
 
 
 def main(global_config, **settings):
@@ -29,26 +35,37 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DB_SESSION.configure(bind=engine)
 
-    session_factory = SignedCookieSessionFactory('itsaseekreet')
+    factory_s = settings['pyramid.session_factory_secret']
+    auth_s = settings['pyramid.authentication_secret']
 
-    config = Configurator(settings=settings)
+    session_factory = SignedCookieSessionFactory(factory_s)
+    auth_policy = AuthTktAuthenticationPolicy(auth_s, callback=groups_finder)
+
+    config = Configurator(settings=settings,
+                          authentication_policy=auth_policy,
+                          root_factory=RootFactory)
     config.include('pyramid_mako')
     config.set_session_factory(session_factory)
     config.add_static_view(name='static', path='pyramidapp:static')
 
     config.add_route('home', '/')
     # Administration route
-    config.add_route('right_list', r'/admin/right')
-    config.add_route('right_list:new', r'/admin/right/{new}')
-    config.add_route('right_delete', r'/admin/right/{uid:\d+}/delete')
+    config.add_route('home_admin', r'/admin/index.html')
 
-    config.add_route('group_list', r'/admin/group')
-    config.add_route('group_list:new', r'/admin/group/{new}')
-    config.add_route('group_delete', r'/admin/group/{uid:\d+}/delete')
+    config.add_route('right_list', r'/admin/right/list.html')
+    config.add_route('right_list:new', r'/admin/right/list-{new}.html')
+    config.add_route('right_delete', r'/admin/right/delete-{uid:\d+}.html')
 
-    config.add_route('user_list', r'/admin/user')
-    config.add_route('user_list:new', r'/admin/user/{new}')
-    config.add_route('user_delete', r'/admin/user/{uid:\d+}/delete')
+    config.add_route('group_list', r'/admin/group/list.html')
+    config.add_route('group_list:new', r'/admin/group/list-{new}.html')
+    config.add_route('group_delete', r'/admin/group/delete-{uid:\d+}.html')
+
+    config.add_route('user_list', r'/admin/user/list.html')
+    config.add_route('user_list:new', r'/admin/user/list-{new}.html')
+    config.add_route('user_delete', r'/admin/user/delete-{uid:\d+}.html')
+
+    config.add_route('login', r'/login.html')
+    config.add_route('logout', r'/logout.html')
 
     config.scan()
 
