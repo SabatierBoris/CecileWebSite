dffeba012b329aa6a0a5a5d7dc6a673a6d634abd
SABATIER Boris <sabatier.boris@gmail.com>
2016-02-28 22:41:02 +0100

    Correct gunicorn binding address

--- runProd.sh
+++ runProd.sh
@@ -2,5 +2,5 @@
 
 alembic -c pyramidapp/config/production.ini upgrade head
 initialize_bd pyramidapp/config/production.ini
-gunicorn -w 16 wsgi
+gunicorn -w 16 -b 0.0.0.0:8080 wsgi
 #pserve pyramidapp/config/production.ini
